582538b223cbbcae184d20b2379eaf2fe5aeaac9
vishalEEE1911 <30978038+vishalEEE1911@users.noreply.github.com>
2017-08-15 18:37:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'art-one.html'));
 });
-app.get('/article_one', function(req,res){
+app.get('/art-one', function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'art-one.html'));
 })
 app.get('/ui/style.css', function (req, res) {
