2fbee303aa70839ba38e42f4d7bc8ec817e2d225
vishalEEE1911 <30978038+vishalEEE1911@users.noreply.github.com>
2017-08-18 19:39:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 //COUNTER CODE
-var button= document.getElementByID("counter");
+var button= document.getElementByID('counter');
 var count=0;
 button.onclick = function(){
     counter=counter+1;
-    var span= document.getElementByid("counter");
+    var span= document.getElementByid('counter');
     sapn.innerHTML = counter.toString();
-}
+};
